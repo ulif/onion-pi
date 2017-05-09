efa341b2a89d2c43d296ccb3bdc09f09fcd25487
ulif <uli@gnufix.de>
2017-05-10 00:11:34 +0200

    Clean up bash script.
    Remove trash and give a better description of what we do here.

--- map_iface_driver.sh
+++ map_iface_driver.sh
@@ -3,10 +3,9 @@
 set -e
 
 MAC=`ls -d /sys/class/net/*/device/driver/module/drivers/* \
-        | cut -d '/' -f "5,10" --output-delimiter " " \
-        | grep "sdio" \
+        | grep "/sdio:brcmfmac" \
         | head -1  \
-        | awk '{ print("/sys/class/net/" $1 "/address");}' \
+        | awk -F "/" '{ print("/sys/class/net/" $5 "/address");}' \
         | xargs cat`
 
 if [ -z "$MAC" ] ; then
@@ -17,4 +16,5 @@ fi
 RULE='SUBSYSTEM=="net", ACTION=="add", DRIVERS=="?*", ATTR{address}=="'$MAC'", '
 RULE+='ATTR{dev_id}=="0x0", ATTR{type}=="1", KERNEL=="wlan*", NAME="wlan0"'
 
+echo "Pinning builtin wifi to name 'wlan0' via udev rule in /etc/udev/rules.d/70-persistent-net.rules"
 echo "$RULE" >> "/etc/udev/rules.d/70-persistent-net.rules"
